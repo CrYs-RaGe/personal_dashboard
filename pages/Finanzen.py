6d16c4d00cf08727cbfe5fc615f8b5db22ef217a
Lars Poll <lars.poll@gmx.de>
2024-04-21 21:32:15 +0200

    Add interval finance overview

--- pages/Finanzen.py
+++ pages/Finanzen.py
@@ -3,6 +3,10 @@ import pandas as pd
 pd.options.mode.copy_on_write=True
 import streamlit as st
 from copy import copy
+import plotly.express as px
+import plotly.graph_objects as go
+from datetime import datetime
+import calendar
 
 from src import utils
 utils.session_state_helper()
@@ -26,9 +30,7 @@ def get_data():
 
     return dfs
 
-def interval(data):
-    years = list(data.dfs.keys())
-
+def interval(years):
     if st.checkbox("Jahre ausschließen"):
         max_year = st.selectbox("Alle Jahre ausschließen bis:", years)
     
@@ -48,6 +50,29 @@ def interval(data):
     end_year = end_date.split()[1]
     end_month = end_date.split()[0]
 
+    return start_year, start_month, end_year, end_month
+
+def yearly(years):
+    start_year = st.selectbox('Jahr', years, index=len(years)-1, key='Jahr_yearly')
+    end_year = start_year
+
+    start_month = 'Januar'
+    end_month = 'Dezember'
+
+    return start_year, start_month, end_year, end_month
+
+def monthly(years):
+    start_year = st.selectbox('Jahr', years, index=len(years)-1, key='Jahr_monthly')
+    end_year = start_year
+
+    months = list(st.session_state.month_map.keys())
+
+    start_month = st.selectbox('Monat', months, index=months.index(calendar.month_name[datetime.now().month]), key='Monat_monthly')
+    end_month = start_month
+
+    return start_year, start_month, end_year, end_month
+
+def build_interval_df(data, start_year, start_month, end_year, end_month):
     months = list(st.session_state.month_map.keys())
 
     column_names_unified = ['Betrag', 'Beschreibung', 'Kategorie']
@@ -76,10 +101,117 @@ def interval(data):
             df = dfs[y][m].rename(columns=dict(zip(dfs[y][m].columns, column_names_unified)))
             interval_df = pd.concat([interval_df, df], ignore_index=True)
 
-    with st.expander('Daten'):
-        st.write(interval_df)
+    return interval_df
 
+def sunburst_chart(interval_df):
+    kategorien = sorted(list(interval_df['Kategorie'].drop_duplicates()))
+    
+    try:
+        kategorien.remove('Gehalt')
+    except:
+        pass
 
+    values = []
+    to_remove = []
+
+    for k in kategorien:
+        summe = interval_df.loc[interval_df['Kategorie']==k, 'Betrag'].sum()
+        if summe > 0:
+            to_remove.append(k)
+        else:
+            values.append(abs(summe))
+
+    for k in to_remove:
+        kategorien.remove(k)
+
+    df = pd.DataFrame(columns=['Kategorie', 'Summe'])
+    df['Kategorie'] = kategorien
+    df['Summe'] = values
+
+    fig = px.sunburst(df, path=['Kategorie'], values='Summe')
+    fig.update_layout(margin=dict(t=0, l=0, r=0, b=0))
+
+    return fig
+
+def waterfall_chart(interval_df):
+    kategorien = sorted(list(interval_df['Kategorie'].drop_duplicates()))
+
+    values = []
+    total = 0
+    measure = []
+
+    for k in kategorien:
+        summe = interval_df.loc[interval_df['Kategorie']==k, 'Betrag'].sum()
+        total = total + summe
+        values.append(round(summe,2))
+        measure.append('relative')
+
+    kategorien.append('Summe')
+    values.append(round(total,2))
+    measure.append('total')
+
+    fig = go.Figure(go.Waterfall(
+        orientation = "v",
+        measure = measure,
+        x = kategorien,
+        textposition = "outside",
+        text = values,
+        y = values,
+        connector = {"line":{"color":"rgb(63, 63, 63)"}},
+    ))
+
+    return fig
+
+def pareto_chart(interval_df):
+    kategorien = sorted(list(interval_df['Kategorie'].drop_duplicates()))
+
+    values = []
+    to_remove = []
+
+    for k in kategorien:
+        summe = interval_df.loc[interval_df['Kategorie']==k, 'Betrag'].sum()
+        if summe > 0:
+            to_remove.append(k)
+        else:
+            values.append(abs(summe))
+
+    for k in to_remove:
+        kategorien.remove(k)
+
+    df = pd.DataFrame(columns=['Kategorie', 'Betrag', 'Betrag single'])
+    df['Kategorie'] = kategorien
+    df['Betrag single'] = values
+    total = sum(values)
+    threshold = 0
+    is_threshold = False
+    df.sort_values(by=['Betrag single'], ascending=False, inplace=True, ignore_index=True)
+
+    for k in range(len(kategorien)):
+        trace_values = []
+
+        for i in range(k):
+            trace_values.append(0.0)
+
+        for i in range(len(kategorien)-k):
+            trace_values.append(round(float(df.loc[k, 'Betrag single']) / total,2))
+
+        threshold += round(float(df.loc[k, 'Betrag single']) / total,2)
+        if threshold > 0.8 and not is_threshold:
+            threshold_index = k
+            is_threshold = True
+        
+        df.loc[k, 'Betrag'] = str(trace_values)
+
+    for k in range(threshold_index+1):
+        if k == 0:
+            fig = go.Figure(go.Bar(x=df.loc[:threshold_index,'Kategorie'], y=eval(df.loc[k,'Betrag']), name=df.loc[k,'Kategorie']))
+        else:
+            fig.add_trace(go.Bar(x=df.loc[:threshold_index,'Kategorie'], y=eval(df.loc[k,'Betrag']), name=df.loc[k,'Kategorie']))
+
+    fig.update_layout(barmode='stack', xaxis={'categoryorder':'total ascending'})
+
+    return fig
+        
 def main():
     st.title("Finanzen")
 
@@ -87,15 +219,41 @@ def main():
 
     data = fin(dfs)
 
-    tabs = st.tabs(['Interval','Other'])
-
-    with tabs[-1]:
-        year = st.selectbox('Jahr', data.dfs.keys())
-        month = st.selectbox('Monat', data.dfs[year].keys())
-        st.write(data.dfs[year][month])
+    tabs = st.tabs(['Addition','Vergleich','Kategorie','Prognose'])
 
     with tabs[0]:
-        interval(data)
+
+        option = st.selectbox('Addition über welche Art von Zeitraum?',['Intervall', 'Jährlich', 'Monat'])
+
+        if option == 'Intervall':
+            start_year, start_month, end_year, end_month = interval(list(data.dfs.keys()))
+
+        elif option == 'Jährlich':
+            start_year, start_month, end_year, end_month = yearly(list(data.dfs.keys()))
+
+        elif option == 'Monat':
+            start_year, start_month, end_year, end_month = monthly(list(data.dfs.keys()))
+
+        interval_df = build_interval_df(data, start_year, start_month, end_year, end_month)
+        with st.expander('Daten'):
+            st.write(interval_df)
+
+        subtabs = st.tabs(['Sunburst', 'Wasserfall', 'Pareto'])    
+
+        with subtabs[0]:
+            st.plotly_chart(sunburst_chart(interval_df))
+
+        with subtabs[1]:
+            st.plotly_chart(waterfall_chart(interval_df))
+
+        with subtabs[2]:
+            st.plotly_chart(pareto_chart(interval_df))        
+
+
+    with tabs[1]:
+        pass
+        # start_year, start_month, end_year, end_month = monthly(list(data.dfs.keys()))
+    
 
 if __name__ == '__main__':
     main()
